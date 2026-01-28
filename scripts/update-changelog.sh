33365eb85be6475614c4c8486b9e4acfa5ae7a37
Lucas Burigo <281664+burigolucas@users.noreply.github.com>
2026-01-28 08:38:05 +0100

    chore: fix changelog linting

--- scripts/update-changelog.sh
+++ scripts/update-changelog.sh
@@ -6,8 +6,12 @@ NEXT_VERSION=$(semantic-release version --print)
 # Create fragments based on commit messages
 . ./scripts/create-changelog-fragments.sh $LAST_VERSION $NEXT_VERSION
 
+export ANSIBLE_COLLECTIONS_PATH=.
+
 # Lint fragments
 antsibull-changelog lint && echo "✅ All changelog fragments passed linting!" || exit 1
 
 # Run antsibull-changelog release to update changelog and delete fragments
 antsibull-changelog release -v --version $NEXT_VERSION || echo "❌ Error: Failed to update changelog" &&  exit 1
+
+echo "✅ Changelog updated successfully!"
